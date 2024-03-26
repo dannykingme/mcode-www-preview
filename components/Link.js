92dde7b109456a67af23311a4f78d0ff86936237
Danny King <danny.newidea@gmail.com>
2024-03-26 14:45:59 -0500

    (project/rule3) Description of commit C.
    https://some.link.to/more/detail

--- components/Link.js
+++ components/Link.js
@@ -1,12 +1,12 @@
 import { default as NextLink } from 'next/link';
 import cn from 'clsx';
 
-export const HyperLink = ({ className, ...rest }) => {
-  return <Link className={cn(className, 'hyperlink')} {...rest} />;
-};
-
 const Link = ({ className, ...rest }) => {
   return <NextLink className={cn(className, 'link')} {...rest} />;
+};
+
+export const HyperLink = ({ className, ...rest }) => {
+  return <Link className={cn(className, 'hyperlink')} {...rest} />;
 };
 
 export default Link;
