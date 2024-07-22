685b5ccff738ec69c6ca82256b8e3ecbe87d9682
Danny King <danny.newidea@gmail.com>
2024-07-22 15:30:55 -0500

    add news articles for press page

--- app/press/page.tsx
+++ app/press/page.tsx
@@ -32,6 +32,58 @@ export default function ContactPage() {
             </div>
           </div>
         </div>
+        <div className="articles-container">
+          <div className="content">
+            <div className="articles-heading">
+              <h1>In the news</h1>
+            </div>
+            <div className="articles">
+              <div className="article">
+                <h6 className="article-type">CNBC</h6>
+                <h2 className="article-title">
+                  Michael Fertik talks AI on CNBC Squawk Box
+                </h2>
+                <div className="article-meta">
+                  <span>7 min watch</span>
+                  <div className="article-meta-external">
+                    <Icon name="external" />
+                  </div>
+                </div>
+                <Link
+                  className="article-handle"
+                  href="https://www.cnbc.com/video/2024/05/28/we-are-still-in-the-hardware-moments-of-the-ai-cycle-says-heroic-venturesa-michael-fertik.html"
+                />
+              </div>
+              <div className="article">
+                <h6 className="article-type">Bloomberg</h6>
+                <h2 className="article-title">
+                  Michael Fertik discusses AI with Francine Lacqua on Bloomberg
+                </h2>
+                <div className="article-meta">
+                  <span>6 min watch</span>
+                  <div className="article-meta-external">
+                    <Icon name="external" />
+                  </div>
+                </div>
+                <Link className="article-handle" href="#" />
+              </div>
+              <div className="article">
+                <h6 className="article-type">CNBC</h6>
+                <h2 className="article-title">
+                  Michael Fertik discusses AI and Silicon Valley on CNBC
+                </h2>
+                <div className="article-meta">
+                  <span>9 min watch</span>
+                  <div className="article-meta-external">
+                    <Icon name="external" />
+                  </div>
+                </div>
+                <Link className="article-handle" href="#" />
+              </div>
+            </div>
+            <div className="articles-scroll"></div>
+          </div>
+        </div>
       </div>
       <Footer />
     </App>
