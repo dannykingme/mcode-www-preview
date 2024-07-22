f0011ad18a3c08abd0153a36d20d88c86acbd385
Danny King <danny.newidea@gmail.com>
2024-07-22 15:00:43 -0500

    flesh out homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,33 +16,39 @@ const Header: FC<HeaderProps> = ({
     <header className="header">
       <div className="nav">
         <div className="nav-items content">
-          <div className="nav-item nav-brand">
+          <Link className="nav-item nav-brand" href="/">
             <Logo />
-            {handleAboutClick ? (
-              <button className="link" onClick={handleAboutClick} />
-            ) : (
-              <Link className="link" href="/" />
-            )}
-          </div>
-          <div className="nav-item">
-            {handleAboutClick ? (
-              <button onClick={handleAboutClick}>About</button>
-            ) : (
-              <Link href="/about">About</Link>
-            )}
-          </div>
-          <div className="nav-item">
-            <Link href="https://blog.modelcode.ai/" target="_blank">
-              Blog
+            <span className="mcode-name display">Modelcode AI</span>
+            {/* <Link className="link" href="/" /> */}
+          </Link>
+          {handleAboutClick ? (
+            <button className="nav-item" onClick={handleAboutClick}>
+              About
+            </button>
+          ) : (
+            <Link className="nav-item" href="/about">
+              About
             </Link>
-          </div>
-          <div className="nav-item">
-            {handleContactClick ? (
-              <button onClick={handleContactClick}>Contact</button>
-            ) : (
-              <Link href="/contact">Contact</Link>
-            )}
-          </div>
+          )}
+          <Link className="nav-item" href="/press">
+            Press
+          </Link>
+          <Link
+            className="nav-item"
+            href="https://blog.modelcode.ai/"
+            target="_blank"
+          >
+            Blog
+          </Link>
+          {handleContactClick ? (
+            <button className="nav-item" onClick={handleContactClick}>
+              Contact
+            </button>
+          ) : (
+            <Link className="nav-item" href="/contact">
+              Contact
+            </Link>
+          )}
         </div>
       </div>
     </header>
